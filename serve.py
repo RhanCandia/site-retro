947e27d9fe3197d94710c585707b9efe8059e9b8
Rhanel Candia <rhancandia@gmail.com>
2025-11-21 20:49:20 +0800

    Initial commit: Retro-Modern Portfolio with Experience page

---
+++ serve.py
@@ -0,0 +1,23 @@
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+DIRECTORY = "."
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+print(f"Serving Retro Portfolio at http://localhost:{PORT}")
+print("Press Ctrl+C to stop.")
+
+# Open the browser automatically
+webbrowser.open(f"http://localhost:{PORT}")
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped.")
